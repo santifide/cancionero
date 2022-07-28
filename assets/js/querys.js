0d07e13789ed2750a964c721ddf5d109f81da768
Santiago Fidelio <santiago.fidelio@web-experto.com.ar>
2022-07-28 12:43:18 -0300

    all works well

--- assets/js/querys.js
+++ assets/js/querys.js
@@ -14,12 +14,22 @@ function bajarTono (){
 
 }
 
-function cargarCancion(){
-
+function cargarCancion(contador){
+    console.log(contador);
+    var idBtnActual =  '#btn-cancion-'+contador;
+    var tituloCancion = $(idBtnActual).text();
+    var cancion = cancioneroAleluya[tituloCancion];
+    $('#containerCancion').html(cancioneroAleluya[tituloCancion]);
 }
 
 function generarBotones (){
-
+    console.log('entró');
+    var contador = 0
+    for (const tituloCancion in cancioneroAleluya) {
+        contador += 1;
+        var btnActual = '<button id="btn-cancion-'+contador+'" onclick="cargarCancion('+contador+')">'+tituloCancion+'</button>';
+        $('#listadoDeCanciones').append(btnActual);
+    }
 }
 
 function scrollToTop (){
@@ -27,5 +37,9 @@ function scrollToTop (){
 }
 
 function toggleAcordes (){
-    
+
+}
+
+function modoNocturno (){
+
 }
